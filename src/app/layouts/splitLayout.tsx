2fc8c7af081193ee525cfd8b81e4cbda4ed82aa4
André <2181206@my.ipleiria.pt>
2024-09-09 14:43:50 +0100

    split layout fixed

--- src/app/layouts/splitLayout.tsx
+++ src/app/layouts/splitLayout.tsx
@@ -11,13 +11,13 @@ const SplitLayout: React.FC<SplitLayoutProps> = ({ div1Content, div2Content,divT
     return (
 
         <div className="flex bg-primary   text-secundary">
-            <div className="flex grow-0  ml-10  mr-32   flex-col bg-primary sticky h-screen  ">
-                <div className="flex-ratio grow items-end">{divTitle}</div>
-                <div className=" grow ml-8 mt-5 mr-20">       {div1Content}
+            <div className="flex   ml-10  mr-32   flex-col bg-primary sticky h-screen  ">
+                <div className="flex h-[61.5%] grow-0 shrink-0  items-end  " >{divTitle}</div>
+                <div className=" flex  grow   ml-8 mt-5 mr-20">       {div1Content}
                 </div>
             </div>
 
-            <div className="flex-ratio flex grow   bg-primary  min-h-screen	 ">
+            <div className="w-[61.5%] flex  grow-0 shrink-0   bg-primary  min-h-screen	">
                 <div className=" grow"> {div2Content}</div>
             </div>
 
